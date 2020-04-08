a951d392960ab8d020e18df346065b97c535ed9f
Harish Paruchuri <31740401+harishparuchuri@users.noreply.github.com>
2020-04-08 10:43:17 +0530

    Add files via upload

---
+++ src/controller/Main.java
@@ -0,0 +1,28 @@
+package controller;
+
+
+
+import java.sql.Connection;
+
+import utility.ConnectionManager;
+
+public class Main {
+	public static void main(String args[]) throws Exception {
+		
+		
+	// Fill your code
+		Connection con=null;
+		con=ConnectionManager.getConnection();
+		
+	
+	
+	if(con!=null)
+	{
+		System.out.println("Connection Established.");
+	}
+	else
+	{
+		System.out.println("Check your Connection.");
+	}
+}
+}
